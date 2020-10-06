1db4e6b1755373b0472f8ba8c968990aad6620c8
Amil <github email address>
2020-10-06 15:09:51 -0600

    created event index view and updated event show view + event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
 
   def index
-
+    @events = Event.all
   end
 
   def new
@@ -20,6 +20,7 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    @events = Event.all
   end
 
 
