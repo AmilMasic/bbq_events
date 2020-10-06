819706f31dbb2c0f66fddd6f0184630b63df2da8
Amil <github email address>
2020-10-05 20:55:58 -0600

    created new and show methods in events controller and corresponding view files

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,19 +3,30 @@ class EventsController < ApplicationController
   def index
 
   end
-  
-  def new
 
+  def new
+    @event = Event.new
   end
 
 
   def create
-
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
   end
 
   def show
-
+    @event = Event.find(params[:id])
   end
 
+
+  private
+
+  def event_params
+    params.require(:event).permit(:eventname, :location)
+  end
 
 end
