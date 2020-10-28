28071af45cf98d702329a38137ab1bfcb19305b5
Amil <github email address>
2020-10-27 18:26:16 -0600

    added logic to destroy event so only creator can delete it

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -29,25 +29,33 @@ class UserEventsController < ApplicationController
 
     def edit
       @user_event = UserEvent.find(params[:id])
+      redirect_to root_path, notice: 'Thou Shalt Nought duuu dat :( ' unless current_user.id == @user_event.user_id
+
+
     end
 
     def update
+      # binding.pry
       @user_event = UserEvent.find_by(id: params[:id])
-      if current_user = @user_event.user
+
         if @user_event.update(user_event_params)
           redirect_to @user_event
         else
           render :edit
         end
-      else
-        flash[:notice] = 'You are not authorized to make this change'
-      end
+
     end
 
     def destroy
-    UserEvent.find(params[:id]).destroy
-    redirect_to events_url
-  end
+      @user_event = UserEvent.find(params[:id])
+    # binding.pry
+      if @user_event.user == current_user
+        @user_event.destroy
+        redirect_to events_path, notice: 'Deleted'
+      else
+        redirect_to events_path, notice: 'You are not authorized to delete this event.'
+      end
+    end
 
     private
 
--- app/helpers/user_events_helper.rb
+++ app/helpers/user_events_helper.rb
@@ -1,2 +1,6 @@
 module UserEventsHelper
+  # this helper is giving an undefined method 'redirect_to' error, it works just fine inside user events controller
+  # def authorized?
+  #   redirect_to root_path, notice: 'Thou Shalt Nought duuu dat :(' unless current_user.id == @user_event.user_id
+  # end
 end
